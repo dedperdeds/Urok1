b5ae7b48a2797e4adafe6f746a66737c56c38807
dedperdeds <116014099+dedperdeds@users.noreply.github.com>
2022-10-17 18:02:46 +0300

    Add files via upload

---
+++ Urok1.py
@@ -0,0 +1,21 @@
+#import biblioteka
+#from biblioteka import *
+
+def func():
+    a = 1000
+    while a > 0:
+        a = a - 7
+        print(a)
+func()
+peremennaya = func()
+print(func())
+
+list = [1, 2, 3]
+ masiv = {}
+ list.append(4)
+ print(list[3])
+
+ a = int(float(0.33333))
+ b = str()
+ c = bool()
+ 
